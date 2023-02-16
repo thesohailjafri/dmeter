eb668d77ced9b16d01cbd7c52cd5241fab46d8f7
sohail jafri <101564364+sohail-webenetic@users.noreply.github.com>
2023-02-16 20:57:23 +0530

    connected branch with client

--- src/components/MenuContainer.jsx
+++ src/components/MenuContainer.jsx
@@ -1,20 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { IoFastFood } from "react-icons/io5";
-import { categories } from "../utils/data";
-import { motion } from "framer-motion";
-import RowContainer from "./RowContainer";
-import { useStateValue } from "../context/StateProvider";
+import React, { useEffect, useState } from 'react'
+import { IoFastFood } from 'react-icons/io5'
+import { motion } from 'framer-motion'
+import RowContainer from './RowContainer'
 
-const MenuContainer = () => {
-  const [filter, setFilter] = useState("chicken");
-
-  const [{ foodItems }, dispatch] = useStateValue();
+const MenuContainer = ({ categories }) => {
+  const [filter, setFilter] = useState('chicken')
+  const foodItems = []
 
   return (
     <section className="w-full my-6" id="menu">
       <div className="w-full flex flex-col items-center justify-center">
         <p className="text-2xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-16 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100 mr-auto">
-          Our Hot Dishes
+          Menu
         </p>
 
         <div className="w-full flex items-center justify-start lg:justify-center gap-8 py-6 overflow-x-scroll scrollbar-none">
@@ -22,32 +19,32 @@ const MenuContainer = () => {
             categories.map((category) => (
               <motion.div
                 whileTap={{ scale: 0.75 }}
-                key={category.id}
+                key={category._id}
                 className={`group ${
-                  filter === category.urlParamName ? "bg-cartNumBg" : "bg-card"
+                  filter === category.urlParamName ? 'bg-cartNumBg' : 'bg-card'
                 } w-24 min-w-[94px] h-28 cursor-pointer rounded-lg drop-shadow-xl flex flex-col gap-3 items-center justify-center hover:bg-cartNumBg `}
                 onClick={() => setFilter(category.urlParamName)}
               >
                 <div
                   className={`w-10 h-10 rounded-full shadow-lg ${
                     filter === category.urlParamName
-                      ? "bg-white"
-                      : "bg-cartNumBg"
+                      ? 'bg-white'
+                      : 'bg-cartNumBg'
                   } group-hover:bg-white flex items-center justify-center`}
                 >
                   <IoFastFood
                     className={`${
                       filter === category.urlParamName
-                        ? "text-textColor"
-                        : "text-white"
+                        ? 'text-textColor'
+                        : 'text-white'
                     } group-hover:text-textColor text-lg`}
                   />
                 </div>
                 <p
                   className={`text-sm ${
                     filter === category.urlParamName
-                      ? "text-white"
-                      : "text-textColor"
+                      ? 'text-white'
+                      : 'text-textColor'
                   } group-hover:text-white`}
                 >
                   {category.name}
@@ -64,7 +61,7 @@ const MenuContainer = () => {
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default MenuContainer;
+export default MenuContainer
