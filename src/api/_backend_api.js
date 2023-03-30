291f94019d146cd8810011100a4f65c9f008503e
sohail jafri <101564364+sohail-webenetic@users.noreply.github.com>
2023-03-31 02:02:24 +0530

    build

--- src/api/_backend_api.js
+++ src/api/_backend_api.js
@@ -1,13 +1,15 @@
-let hostname = window.location.hostname;
-const localUrl = "http://localhost:5000/api";
-const liveUrl = "https://dmeter-api.herokuapp.com/api";
+let hostname = window.location.hostname
+const localUrl = process.env.REACT_APP_LOCAL_SERVER
+const liveUrl = process.env.REACT_APP_LIVE_SERVER
 
-let _backend_api;
+let _backend_api
 
-if (hostname === "localhost") {
-    _backend_api = localUrl;
+if (hostname === 'localhost') {
+  _backend_api = localUrl
 } else {
-    _backend_api = liveUrl;
+  _backend_api = liveUrl
 }
 
-export default _backend_api;
+_backend_api += 'api'
+
+export default _backend_api
